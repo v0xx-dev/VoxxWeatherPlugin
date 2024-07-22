e54b29d9630cadf45eebdabe48d00a2787d4ff79
nexxod <duxyoqv0x@mozmail.com>
2024-07-22 14:34:48 +0200

    Add project files.

---
+++ DerelictMoonPlugin/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace DerelictMoonPlugin
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "DerelictMoonPlugin";
+        public const string PLUGIN_NAME = "DerelictMoonPlugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Arcadia Plugins
+A helper mod to add some custom functionality to my maps
